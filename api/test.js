1f20d49e3570b18c6d3961a612e7af639eab1105
kimonschubert-cmyk <kimonschubert@gmail.com>
2026-09-20 03:23:29 +0200

    Update test.js

--- api/test.js
+++ api/test.js
@@ -1,6 +1,40 @@
-module.exports = (req, res) => {
-res.status(200).json({
-status: "ok",
-message: "LOS API funktioniert"
+module.exports = async (req, res) => {
+try {
+const apiKey = process.env.GOOGLE_PLACES_API_KEY;
+
+if (!apiKey) {
+  return res.status(500).json({
+    error: "GOOGLE_PLACES_API_KEY fehlt"
+  });
+}
+const query =
+  req.query?.query ||
+  "Spielplätze in Bad Kissingen";
+const response = await fetch(
+  "https://places.googleapis.com/v1/places:searchText",
+  {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "X-Goog-Api-Key": apiKey,
+      "X-Goog-FieldMask":
+        "places.id,places.displayName,places.formattedAddress,places.location,places.photos,places.googleMapsUri,places.rating,places.websiteUri"
+    },
+    body: JSON.stringify({
+      textQuery: query,
+      languageCode: "de",
+      regionCode: "DE",
+      pageSize: 10
+    })
+  }
+);
+const data = await response.json();
+return res.status(response.status).json(data);
+
+} catch (error) {
+return res.status(500).json({
+error: “Google Places Anfrage fehlgeschlagen”,
+details: error.message
 });
+}
 };
